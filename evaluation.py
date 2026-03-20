273fddb42bc07c9bb1958f5807fe7c2c5194bc63
Karl Wenzel <62113476+karl-wenzel@users.noreply.github.com>
2026-03-20 10:22:30 +0100

    Test eval without simulate_stream

--- evaluation.py
+++ evaluation.py
@@ -84,7 +84,7 @@ def evaluate():
         results = transcribe(
             model=model,
             wav_file=wav_path,
-            simulate_stream=True,
+            simulate_stream=False,
             language="en" if not args.multilingual else "auto",
             beam_size=5,
             temperature=0,
